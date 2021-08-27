cab425b1ead4d9e1688317f81b239f35264b42db
patrice paillard <paillard.patrice@gmail.com>
2021-08-27 08:44:06 +0200

    server et app/ cnx BDD

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+// connexion à la BDD MongoDB Atlas
+mongoose.connect('mongodb+srv://hottakes:Tk7HnepakfcFNDn@cluster0.mooxj.mongodb.net/HotTakes?retryWrites=true&w=majority',
+    {  useNewUrlParser: true,
+       useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+app.use(bodyParser.json());
+
+
+module.exports = app;
